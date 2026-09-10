56e90936dec03a42270a72ffe6dc60503bc2c1e3
migodam <huijiezheng03@gmail.com>
2026-09-11 04:24:58 +0800

    Expose static interval API; delivery audit records the separately blocked core file

---
+++ research/q5_continuum_v2c/interval_array.py
@@ -0,0 +1,4 @@
+"""Public interval API; implementation is split into readable static modules."""
+from certified_modal import I
+from interval_core import (DT,PI,R,C,down,up,rational_endpoint,stack,cstack,pi_integer,sincos)
+from interval_special import (j_regular,h_outgoing,norm_lm,gauss_certified,spherical_quadrature)
